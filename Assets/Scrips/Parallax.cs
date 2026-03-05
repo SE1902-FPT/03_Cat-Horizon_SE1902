c90082dbb5fc9fe587656a46b0145bc7fe79e0b3
TrungKA <trungkahe170409@fpt.edu.vn>
2026-03-05 23:55:01 +0700

    background

---
+++ Assets/Scrips/Parallax.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private Material material;
+    [SerializeField] 
+    private float parallaxFactor = 0.01f;
+    private float offset;
+    public float gameSpeed = 5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ParallaxScroll();
+    }
+
+    private void ParallaxScroll()
+    {
+        // 1. Lấy tốc độ từ GameManager (nếu có) để đồng bộ
+        float speed = gameSpeed * parallaxFactor;
+
+        // 2. Tính toán tốc độ trôi dựa trên hệ số Parallax
+        offset += Time.deltaTime * speed;
+
+        
+
+        // 4. Set lại offset cho Texture (SỬA LỖI Ở ĐÂY: _MainTex)
+        material.SetTextureOffset("_MainTex", Vector2.right * offset);
+    }
+}
