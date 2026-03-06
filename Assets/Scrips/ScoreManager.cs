36fbe8eb1984e3607bccf0315cd6851676508112
TrungKA <trungkahe170409@fpt.edu.vn>
2026-03-06 11:35:19 +0700

    score and check kill enemy

---
+++ Assets/Scrips/ScoreManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using TMPro; // Sử dụng TextMeshPro để hiển thị điểm
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    private int score = 0;
+    [SerializeField] private TextMeshProUGUI scoreText;
+
+    void Awake()
+    {
+        // Singleton để dễ dàng truy cập từ các script khác
+        instance = this;
+    }
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
